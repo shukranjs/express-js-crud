8505be547fd9c7ed956ba04c298d3d2b0d964239
shukranjs <shukranjsp@gmail.com>
2024-08-23 17:46:40 +0400

    changed default port to 5000

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    port: process.env.PORT || 6000,
+    port: process.env.PORT || 5000,
     mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/express-crud-auth',
     jwtSecret: process.env.JWT_SECRET || 'your_secret_key',
 };
